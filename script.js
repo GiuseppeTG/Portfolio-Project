d0dd287d3e5b1e2bceaac8ce3ee68326d0f86a0c
Alfred Boateng <programmingwithalfred@gmail.com>
2022-05-09 15:16:11 +0000

    Update mobile menu

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+const header = document.querySelector('header');
+const body = document.querySelector('body');
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  header.classList.add('full-screen');
+  body.classList.add('no-scroll');
+});
 
-})
-
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}))
+document.querySelectorAll('.nav-link').forEach((n) =>
+  n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+    header.classList.remove('full-screen');
+    body.classList.remove('no-scroll');
+  })
+);
