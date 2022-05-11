720b1da492ebc8c108487dafc71dcbb9f00b6837
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-05-11 08:59:24 -0500

    Linters

--- script.js
+++ script.js
@@ -4,53 +4,58 @@ const projects = [
   {
     projectTitle: 'Project #0',
     description: 'project#0 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
+    mainImage: 'images/project-1.jpg',
     images: ['images/project-1.jpg', 'images/project-4.jpg', 'images/project-3.jpg', 'images/project-2.jpg'],
     languages: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
     linkToLiveVersion: '#',
-    linkToSource: '#',
+    linkToSource: 'https://github.com/GiuseppeTG',
   },
   {
     projectTitle: 'Project #1',
     description: 'project#1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
+    mainImage: 'images/project-2.jpg',
     images: ['images/project-2.jpg', 'images/project-4.jpg', 'images/project-3.jpg', 'images/project-2.jpg'],
     languages: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
     linkToLiveVersion: '#',
-    linkToSource: '#',
+    linkToSource: 'https://github.com/GiuseppeTG',
   },
   {
     projectTitle: 'Project #2',
     description: 'project#2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
+    mainImage: 'images/project-3.jpg',
     images: ['images/project-3.jpg', 'images/project-4.jpg', 'images/project-3.jpg', 'images/project-2.jpg'],
     languages: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
     linkToLiveVersion: '#',
-    linkToSource: '#',
+    linkToSource: 'https://github.com/GiuseppeTG',
   },
   {
     projectTitle: 'Project #3',
     description: 'project#3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
+    mainImage: 'images/project-4.jpg',
     images: ['images/project-4.jpg', 'images/project-4.jpg', 'images/project-3.jpg', 'images/project-2.jpg'],
     languages: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
     linkToLiveVersion: '#',
-    linkToSource: '#',
+    linkToSource: 'https://github.com/GiuseppeTG',
   },
   {
     projectTitle: 'Project #4',
     description: 'project#4 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
+    mainImage: 'images/project-5.jpg',
     images: ['images/project-5.jpg', 'images/project-4.jpg', 'images/project-3.jpg', 'images/project-2.jpg'],
     languages: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
     linkToLiveVersion: '#',
-    linkToSource: '#',
+    linkToSource: 'https://github.com/GiuseppeTG',
   },
   {
     projectTitle: 'Project #5',
     description: 'project#5 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
+    mainImage: 'images/project-6.jpg',
     images: ['images/project-6.jpg', 'images/project-4.jpg', 'images/project-3.jpg', 'images/project-2.jpg'],
     languages: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
     linkToLiveVersion: '#',
-    linkToSource: '#',
+    linkToSource: 'https://github.com/GiuseppeTG',
   },
 ];
-
 
 // constants
 
@@ -60,7 +65,6 @@ const header = document.querySelector('header');
 const body = document.querySelector('body');
 const modal = document.querySelector('#modal');
 
-
 // mobile menu
 
 hamburger.addEventListener('click', () => {
@@ -68,38 +72,43 @@ hamburger.addEventListener('click', () => {
   navMenu.classList.toggle('active');
   header.classList.toggle('full-screen');
   body.classList.toggle('no-scroll');
-})
+});
 
 document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
   header.classList.remove('full-screen');
   body.classList.remove('no-scroll');
-}))
-
+}));
 
 // modal window
 
-function openModal(num) {
-
-    const languages = projects[num].languages;
+function openModal(num = null) {
+  if (num != null) {
+    const { languages } = projects[num];
     let languagesList = '';
-    languages.forEach( (listedItem) => { languagesList += `<li>${listedItem}</li>` });
+    languages.forEach((listedItem) => { languagesList += `<li>${listedItem}</li>`; });
 
-    const images = projects[num].images;
+    const { images } = projects[num];
     let imagesList = '';
-    images.forEach( (listedImage) => { imagesList += `<img src="${listedImage}"></img>` });
+    images.forEach((listedImage) => { imagesList += `<img src="${listedImage}"></img>`; });
 
     document.getElementById('modal-project-name').innerHTML = projects[num].projectTitle;
     document.getElementById('modal-languages').innerHTML = languagesList;
-    document.getElementById('foto').src = projects[num].images[0];
+    document.getElementById('foto').src = projects[num].mainImage;
     document.getElementById('modal-img-preview').innerHTML = imagesList;
-    document.getElementById('modal-project-description').innerHTML = projects[num].description;
-    document.getElementById('modal-project-live-version').href = projects[num].linkToLiveVersion;
+    document.getElementById('modal-project-live').href = projects[num].linkToLiveVersion;
+    document.getElementById('modal-project-source').href = projects[num].linkToSource;
 
     modal.classList.add('active');
+  }
 }
 
-function closeModal() {
-  modal.classList.remove('active');
+function closeModal(id = null) {
+  if (id != null) {
+    modal.classList.remove('active');
+  }
 }
+
+openModal();
+closeModal();
